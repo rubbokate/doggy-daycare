fd74d3ccd4223028e70ae0db52de153b100bf683
Kaitlyn Rubbo <kaitlynrubbo@gmail.com>
2021-05-03 21:29:25 -0400

    adding react app changes

---
+++ happy-paws/src/Board.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Board = () => (
+        <div>
+
+
+        </div>
+        )
---
+++ happy-paws/src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Photo from './img/IMG_0015.JPG';
+
+export const Home = () => (
+        <div>
+<img src="./img/IMG_0015.JPG"/>
+<h2>Home Page</h2>
+        </div>
+        )
