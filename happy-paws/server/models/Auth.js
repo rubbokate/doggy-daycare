17e8b1f4c32d4ef44a341dd64bc7b76f78fe8efd
Ryan Lackey <lackeyr13@gmail.com>
2021-05-10 19:00:03 -0400

    Models

---
+++ happy-paws/server/models/Auth.js
@@ -0,0 +1,20 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const ThirdPartyProviderSchema = new Schema({
+//   provider_name: {
+//     type: String,
+//     default: null,
+//   },
+//   provider_id: {
+//     type: String,
+//     default: null,
+//   },
+//   provider_data: {
+//     type: {},
+//     default: null,
+//   },
+// }); 
+
+// const Auth = mongoose.model("auth", ThirdPartyProviderSchema)
+// module.exports = Auth;
