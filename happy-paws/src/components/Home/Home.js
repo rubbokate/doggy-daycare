f8098465e39c78d10df1cb0e5d16f296763f0835
Kaitlyn Rubbo <kaitlynrubbo@gmail.com>
2021-05-10 20:19:31 -0400

    adding button and other things to home

--- happy-paws/src/components/Home/Home.js
+++ happy-paws/src/components/Home/Home.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import Pupcake from '../data/icons/pupcake-1.jpeg';
-import Jersery from '../data/icons/jersery-1.jpeg';
+import jersery from '../data/icons/jersery-1.jpeg';
+import '../style.css';
+import { Button } from 'react-bootstrap';
 
 
+export const Home = () => {
+        return (
+        <div id="bookingtext">
 
-export const Home = () => (
-        <div>
-<img id="jersery" alt="chihuahua in a blanket" src={Jersery}/>
-<img id="pupcake" alt="pupcake" src={Pupcake}/>
+    <div id="jersery2">
+        <img src="{jersery}" alt="chihuahua"/>
+    </div>
 
+    <div id="texts"> 
+    Need to book your dogs' stay with us?
+    </div>
+    <div>
+    <Button variant="danger" size="lg" active>
+    Book Now
+  </Button>
+    </div>
 
-        </div>
-        )
+</div>
+        );
+}
