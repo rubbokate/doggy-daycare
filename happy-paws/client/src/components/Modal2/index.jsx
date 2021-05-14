910e14e39dd46dbb08b80174ab68a9768b1c4e61
James <jamesgault1488@gmail.com>
2021-05-14 18:32:12 -0400

    addded Modal2 folder

---
+++ happy-paws/client/src/components/Modal2/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+function Modal() {
+    return (
+        <div>
+            <form action="/login" method="post">
+    <div>
+        <label>Username:</label>
+        <input type="text" name="username"/>
+    </div>
+    <div>
+        <label>Password:</label>
+        <input type="password" name="password"/>
+    </div>
+    <div>
+        <input type="submit" value="Log In"/>
+    </div>
+</form>
+        </div>
+    ) 
+}
+
+export default Modal;
