9650f5f4dbde25b107c9a403ab48f9be1466f72c
James <jamesgault1488@gmail.com>
2021-05-14 13:51:08 -0400

    changed train.js to reservations.js

--- happy-paws/server/passport/.credentials.js
+++ happy-paws/server/passport/.credentials.js
@@ -1,11 +1,22 @@
 // Create an anonymous credential
-const credentials = Realm.Credentials.anonymous();
-try {
-  const user = await app.logIn(credentials);
-  console.log("Successfully logged in!", user.id);
-  return user;
-} catch (err) {
-  console.error("Failed to log in", err.message);
-}
+// const credentials = Realm.Credentials.anonymous();
+// try {
+//   const user = await app.logIn(credentials);
+//   console.log("Successfully logged in!", user.id);
+//   return user;
+// } catch (err) {
+//   console.error("Failed to log in", err.message);
+// }
 
-export default credentials;
+// export default credentials;
+
+const passport = require("passport")
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'passwd'
+},
+function(username, password, done) {
+  // ...
+}
+));
