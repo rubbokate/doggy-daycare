55d40b782fc54518096dd24ab326d15e3049013d
James <jamesgault1488@gmail.com>
2021-05-14 18:28:17 -0400

    Changed Reservation Folder in components

--- happy-paws/server/models/Rooms.js
+++ happy-paws/server/models/Rooms.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose"); 
-const Schema = mongoose.Schema; 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const RoomsSchema = new Schema({
   id: {
@@ -14,28 +14,28 @@ const RoomsSchema = new Schema({
   roomStatus: {
     type: Boolean
   },
- referral_code: {
-   type: String,
-   default: function () {
-     let hash = 0;
-     for (let i = 0; i < this.email.length; i++) {
-       hash = this.email.charCodeAt(i) + ((hash << 5) - hash);
-     }
-     let res = (hash & 0x00ffffff).toString(16).toUpperCase();
-     return "00000".substring(0, 6 - res.length) + res;
-   }
- },
-   referred_by: {
-     type: String,
-     default: null
-   },
-//     third_party_auth: [ThirdPartyProviderSchema],
-//    date: {
-//      type: Date,
-//      default: Date.now
-//    }, 
+  referral_code: {
+    type: String,
+    default: function () {
+      let hash = 0;
+      for (let i = 0; i < this.email.length; i++) {
+        hash = this.email.charCodeAt(i) + ((hash << 5) - hash);
+      }
+      let res = (hash & 0x00ffffff).toString(16).toUpperCase();
+      return "00000".substring(0, 6 - res.length) + res;
+    }
+  },
+  referred_by: {
+    type: String,
+    default: null
+  },
+  //     third_party_auth: [ThirdPartyProviderSchema],
+  //    date: {
+  //      type: Date,
+  //      default: Date.now
+  //    }, 
 },
- { strict: false }
+  { strict: false }
 );
 const Rooms = mongoose.model("rooms", RoomsSchema);
 module.exports = Rooms;
