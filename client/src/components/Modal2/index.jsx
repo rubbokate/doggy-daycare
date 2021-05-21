aa748890e341b23253a9a61cfb3c23c8eda515e1
James <jamesgault1488@gmail.com>
2021-05-20 21:49:24 -0400

    updated for deploy

--- client/src/components/Modal2/index.jsx
+++ client/src/components/Modal2/index.jsx
@@ -1,17 +1,17 @@
 
-import React from 'react';
-import MailchimpSubscribe from "react-mailchimp-subscribe"
+// import React from 'react';
+// import MailchimpSubscribe from "react-mailchimp-subscribe"
 
 
-function Login(props) {
-    return(
-        <div style={{color: "white"}}>
+// function Login(props) {
+//     return(
+//         <div style={{color: "white"}}>
 
-Subscribe To Newsletter Here
-<MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
+// Subscribe To Newsletter Here
+// <MailchimpSubscribe url={process.env.REACT_APP_MAILCHIMP_URL} />
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default Login;
+// export default Login;
